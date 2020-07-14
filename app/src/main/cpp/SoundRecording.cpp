20d2146907f38bd5f56bfacba55cd823462de7ce
Ashfaq Salehin <ashfaq.aws@gmail.com>
2020-07-15 00:40:43 +0700

    Gain factor while writing file

--- app/src/main/cpp/SoundRecording.cpp
+++ app/src/main/cpp/SoundRecording.cpp
@@ -8,8 +8,14 @@
 #include "logging_macros.h"
 
 int32_t SoundRecording::write(const int16_t *sourceData, int32_t numSamples, char* recordingFilePath) {
+    auto *buffer = new int16_t[numSamples]{0};
+
+    for (int i = 0; i < numSamples; i++) {
+        buffer[i] = gain_factor * sourceData[i];
+    }
+
     FILE *f = fopen(recordingFilePath, "ab");
-    fwrite(sourceData, sizeof(*sourceData), numSamples, f);
+    fwrite(buffer, sizeof(*buffer), numSamples, f);
     fclose(f);
     return numSamples;
 }
