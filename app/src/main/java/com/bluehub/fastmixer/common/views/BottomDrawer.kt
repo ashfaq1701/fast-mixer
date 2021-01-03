8f1bc54fbbb167a1310fd354f5ed5d7122652ebf
Ashfaq Salehin <ashfaq@opn.ooo>
2021-01-03 23:42:24 +0700

    Bottom drawer added

--- app/src/main/java/com/bluehub/fastmixer/common/views/BottomDrawer.kt
+++ app/src/main/java/com/bluehub/fastmixer/common/views/BottomDrawer.kt
@@ -7,9 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.transition.Slide
+import androidx.transition.Transition
+import androidx.transition.TransitionManager
 import com.bluehub.fastmixer.R
 import com.bluehub.fastmixer.databinding.BottomDrawerBinding
 import io.reactivex.rxjava3.subjects.BehaviorSubject
+
 
 class BottomDrawer(context: Context, attributeSet: AttributeSet?)
     : ConstraintLayout(context, attributeSet) {
@@ -19,6 +23,8 @@ class BottomDrawer(context: Context, attributeSet: AttributeSet?)
 
     private val drawerOpen: BehaviorSubject<Boolean> = BehaviorSubject.create()
 
+    private lateinit var transition: Transition
+
     init {
         val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
         binding = BottomDrawerBinding.inflate(inflater, this, true)
@@ -26,14 +32,17 @@ class BottomDrawer(context: Context, attributeSet: AttributeSet?)
 
         setupObservers()
         setupViewEvents()
+        setupAnimations()
     }
 
     private fun setupObservers() {
         drawerOpen.subscribe {
             if (it) {
+                TransitionManager.beginDelayedTransition(binding.mainContainer, transition)
                 binding.drawerContainer.visibility = View.VISIBLE
                 binding.drawerControl.setImageResource(R.drawable.drawer_control_button_close)
             } else {
+                TransitionManager.beginDelayedTransition(binding.mainContainer, transition)
                 binding.drawerContainer.visibility = View.GONE
                 binding.drawerControl.setImageResource(R.drawable.drawer_control_button_open)
             }
@@ -50,13 +59,21 @@ class BottomDrawer(context: Context, attributeSet: AttributeSet?)
         }
     }
 
+    private fun setupAnimations() {
+        transition = Slide()
+        transition.duration = 400
+        transition.addTarget(R.id.drawerContainer)
+    }
+
     override fun addView(
         child: View?,
         index: Int,
         params: ViewGroup.LayoutParams?
     ) {
         child?.let { childView ->
-            if (!listOf(R.id.mainContainer, R.id.drawerControl, R.id.drawerContainer).contains(childView.id)) {
+            if (!listOf(R.id.mainContainer, R.id.drawerControl, R.id.drawerContainer).contains(
+                    childView.id
+                )) {
                 return mContainerView.addView(childView, index, params)
             }
         }
