fe4529e22c943fd060020d7f7c9cdb038b272d22
Ashfaq Salehin <ashfaq@opn.ooo>
2021-02-22 02:50:18 +0700

    Shift audio - another checkpoint

--- app/src/main/java/com/bluehub/fastmixer/screens/mixing/modals/ShiftViewModel.kt
+++ app/src/main/java/com/bluehub/fastmixer/screens/mixing/modals/ShiftViewModel.kt
@@ -1,13 +1,18 @@
 package com.bluehub.fastmixer.screens.mixing.modals
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import android.content.Context
+import androidx.lifecycle.*
+import com.bluehub.fastmixer.R
+import com.bluehub.fastmixer.common.config.Config
 import com.bluehub.fastmixer.common.models.AudioFileUiState
 import com.bluehub.fastmixer.screens.mixing.FileWaveViewStore
 import com.bluehub.fastmixer.screens.mixing.MixingRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class ShiftViewModel@Inject constructor(
+    val context: Context,
     val fileWaveViewStore: FileWaveViewStore,
     val mixingRepository: MixingRepository
 ) : BaseDialogViewModel() {
@@ -29,7 +34,36 @@ class ShiftViewModel@Inject constructor(
     }
 
     fun saveShift() {
+        _audioFileUiState.run {
+            if (!playSliderPosition.hasValue()) {
+                errorLiveData.value = context.getString(R.string.error_play_head_required)
+                return
+            }
+
+            if (playSliderPosition.value > numSamples) {
+                errorLiveData.value = context.getString(R.string.error_play_head_should_be_less_than_numsamples)
+                return
+            }
+
+            _shiftDuration.value?.let {
+
+                val shiftDurationSamples = (it * (Config.SAMPLE_RATE.toFloat() / 1000.0)).toInt()
+                applyShift(path, playSliderPosition.value, shiftDurationSamples)
+            } ?: run {
+                errorLiveData.value = context.getString(R.string.error_shift_duration_required)
+                return
+            }
+        }
+
         closeDialogLiveData.value = true
+    }
+
+    private fun applyShift(filePath: String, position: Int, numSamples: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            mixingRepository.shiftBySamples(filePath, position, numSamples)
+            // TODO Fetch sample count and update other related params
+            closeDialogLiveData.postValue(true)
+        }
     }
 
     fun cancelShift() {
